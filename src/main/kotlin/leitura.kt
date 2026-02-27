c271356b24ecc55a7a4a78c7c21b2f118eeb2c85
Feyre <inaraoenning@gmail.com>
2026-02-26 21:35:39 -0300

    Add Anotações leitura e variaveis

---
+++ src/main/kotlin/leitura.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Informe seu nome: ")
+    val nome = readln();
+
+    println("${nome} seja bem-vindo!")
+
+}
--- src/main/kotlin/variaveis.kt
+++ src/main/kotlin/variaveis.kt
@@ -7,4 +7,9 @@ fun main() {
 
     println("${nome} tem ${idade} anos!")
 
+
+    val nome1 = "Huilson2"  // Variavel imutável, o tipo é inferido
+    val nome2: String = "Huilson" // Variavel imutável, o tipo é declarado
+    var sobrenome: String; // Variavel mutável, o tipo é declarado
+
 }
